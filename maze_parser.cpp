dd5c80c772689b235e94803293b4b4b50e702251
jlu368 <jylu3@illinois.edu>
2016-09-11 14:00:50 -0500

    Created node class

---
+++ maze_parser.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+class Node {
+	Node left;
+	Node right;
+	Node top;
+	Node bottom;
+	bool point;
+
+	public Node(){
+		left = right = top = bottom = NULL;
+		point = false;	
+	}	
+}
+
+void main(){
+
+}
+
+
+
